433ae4535cf5a5c521fbf5b176fd3d89dc4fec7b
Marc J <john.doe.gmail.com>
2021-02-03 10:12:40 +0100

    Profiles environment variable in 'Edit Configurations'

---
+++ src/test/java/be/hvwebsites/profiles/repositories/DataSourceTest.java
@@ -0,0 +1,24 @@
+package be.hvwebsites.profiles.repositories;
+
+import org.hibernate.annotations.Target;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import javax.sql.DataSource;
+import java.sql.SQLException;
+
+@DataJpaTest
+public class DataSourceTest {
+    private final DataSource dataSource;
+
+    public DataSourceTest(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    @Test
+    void databaseNaam() throws SQLException{
+        try (var connection = dataSource.getConnection()){
+            System.out.println(connection.getCatalog());
+        }
+    }
+}
